30cae1898288a74f4287298a14dfbce55f854c4a
weisong0908 <hi@tengweisong.com>
2019-08-09 13:59:28 +0800

    added vuex

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,10 +1,17 @@
 <template>
-    <h1>Dashboard</h1>
+	<div>
+		<h1>Dashboard</h1>
+		<p>Hello {{ name }}</p>
+	</div>
 </template>
 
 <script>
 export default {
-    
-}
+	data() {
+		return {
+			name: this.$store.state.user.name
+		};
+	}
+};
 </script>
 
