17e25622456756cead8e651b4533f13aa28a02af
weisong0908 <hi@tengweisong.com>
2019-08-09 16:56:57 +0800

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
 	<div id="app">
+		<navbar></navbar>
 		<router-view></router-view>
 	</div>
 </template>
+
+<script>
+import navbar from "./components/Navbar";
+
+export default {
+	components: {
+		navbar
+	}
+};
+</script>
