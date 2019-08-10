8536bab8d690d5bceaf45cfb0d32ef2eeaa60b14
weisong0908 <hi@tengweisong.com>
2019-08-10 14:47:08 +0800

    added placeholder for tab content

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -3,11 +3,22 @@
 		<p>Hello {{ name }}</p>
 		<section>
 			<b-tabs position="is-centered" type="is-toggle" expanded>
-				<b-tab-item label="Pictures">pictures</b-tab-item>
+				<b-tab-item label="Overview">
+					<ul>
+						<li>You owe</li>
+						<li>You are owed</li>
+					</ul>
+				</b-tab-item>
 
-				<b-tab-item label="Music">music</b-tab-item>
-
-				<b-tab-item label="Books">books</b-tab-item>
+				<b-tab-item label="Recent activities">
+					a transactions table with
+					<ul>
+						<li>Name</li>
+						<li>Date time of transaction</li>
+						<li>purpose</li>
+						<li>link to detail</li>
+					</ul>
+				</b-tab-item>
 			</b-tabs>
 		</section>
 	</page>
