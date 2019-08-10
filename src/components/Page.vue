af02ae3aa74bc0fe5206bc7f3aaebedab19d48d1
weisong0908 <hi@tengweisong.com>
2019-08-10 14:20:15 +0800

    added common message

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,14 +1,17 @@
 <template>
-	<div class="tile is-vertical is-ancestor">
-		<div class="tile is-parent">
-			<div class="tile is-child">
-				<p class="title">{{ title }}</p>
-				<p class="subtitle">{{ subtitle }}</p>
+	<div>
+		<b-message title="Warning" type="is-warning" has-icon>This page is under development</b-message>
+		<div class="tile is-vertical is-ancestor">
+			<div class="tile is-parent">
+				<div class="tile is-child">
+					<p class="title">{{ title }}</p>
+					<p class="subtitle">{{ subtitle }}</p>
+				</div>
 			</div>
-		</div>
-		<div class="tile is-parent">
-			<div class="tile is-child">
-				<slot></slot>
+			<div class="tile is-parent">
+				<div class="tile is-child">
+					<slot></slot>
+				</div>
 			</div>
 		</div>
 	</div>
