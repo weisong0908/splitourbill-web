3b57375b9cb61216eeb90e9b671ce4bf0c2e9a37
Teng Wei Song <hi@tengweisong.com>
2019-08-25 17:46:32 +0800

    added transaction detail page

---
+++ src/pages/TransactionDetail.vue
@@ -0,0 +1,37 @@
+<template>
+    <page title="Transaction detail">
+        <p>Transaction id: {{transaction.id}}</p>
+        <p>Transaction to: {{transaction.name}}</p>
+        <p>Amount: {{transaction.amount}}</p>
+        <p>Date: {{transaction.date}}</p>
+        <p>Purpose: {{transaction.purpose}}</p>
+        <p>Before: Friend1 owes you: 21.50</p>
+        <p>After: Friend1 owes you: 20.00</p>
+    </page>
+</template>
+
+<script>
+import page from "../components/Page";
+
+export default {
+    components: {
+        page
+    },
+    data() {
+        return {
+            test: "124",
+            transaction: {}
+        };
+    },
+    created() {
+        this.transaction = {
+            id: 1,
+            name: "Friend 1",
+            amount: 2.5,
+            date: "2019-8-2",
+            purpose: "you paid Friend 1",
+            detail: "/detail/1"
+        };
+    }
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -2,13 +2,15 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 import Dashboard from "./pages/Dashboard";
+import TransactionDetail from "./pages/TransactionDetail";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    routes: [
-        { name: "dashboard", path: "/dashboard", component: Dashboard },
-        { path: "/home", redirect: "dashboard" },
-        { path: "/", redirect: "dashboard" }
-    ]
-})
+	routes: [
+		{ name: "dashboard", path: "/dashboard", component: Dashboard },
+		{ path: "/home", redirect: "dashboard" },
+		{ name: "transactionDetail", path: "/detail/:id", component: TransactionDetail },
+		{ path: "/", redirect: "dashboard" }
+	]
+});
