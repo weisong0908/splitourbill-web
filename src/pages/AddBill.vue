0d9f9659b018b996cc8487c57ec46bcb5c389047
weisong0908 <hi@tengweisong.com>
2019-09-01 16:49:36 +0800

    added basic input fields

--- src/pages/AddBill.vue
+++ src/pages/AddBill.vue
@@ -1,8 +1,52 @@
 <template>
 	<page title="Add a bill">
 		<section>
-			<b-field label="Title">
-				<b-input v-model="title"></b-input>
+			<p>Purpose: {{purpose}}</p>
+			<p>Amount: {{amount}}</p>
+			<p>Date: {{date}}</p>
+			<p>Time: {{time}}</p>
+			<p>Message: {{message}}</p>
+		</section>
+		<section>
+			<b-field label="Purpose">
+				<b-select placeholder="What is this bill for?" v-model="purpose">
+					<optgroup label="Meal">
+						<option value="breakfast">Breakfast</option>
+						<option value="lunch">Lunch</option>
+						<option value="dinner">Dinner</option>
+						<option value="snack">Snack</option>
+						<option value="drink">Drink</option>
+						<option value="brunch">Brunch</option>
+						<option value="supper">Supper</option>
+					</optgroup>
+					<optgroup label="Activity">
+						<option value="movie">Movie</option>
+						<option value="singK">Sing K</option>
+						<option value="escapeRoom">Escape room</option>
+						<option value="workout">Workout</option>
+					</optgroup>
+					<optgroup label="Event">
+						<option value="weddingBanquet">Wedding banquet</option>
+						<option value="songka">Songka</option>
+					</optgroup>
+				</b-select>
+			</b-field>
+			<b-field label="Amount">
+				<b-input v-model="amount" type="number"></b-input>
+			</b-field>
+			<b-field label="Select a date">
+				<b-datepicker
+					v-model="date"
+					:date-formatter="dateFormat"
+					placeholder="Click to select..."
+					icon="calendar-today"
+				></b-datepicker>
+			</b-field>
+			<b-field label="Select a time">
+				<b-timepicker v-model="time" placeholder="Click to select..." icon="clock"></b-timepicker>
+			</b-field>
+			<b-field label="Message">
+				<b-input type="textarea" v-model="message"></b-input>
 			</b-field>
 		</section>
 	</page>
@@ -17,7 +61,13 @@ export default {
 	},
 	data() {
 		return {
-			title: ""
+			purpose: "",
+			amount: "",
+			dateTime: new Date(),
+			dateFormat: date => date.toLocaleDateString("en-SG"),
+			date: dateTime,
+			time: dateTime,
+			message: "A new bill"
 		};
 	}
 };
