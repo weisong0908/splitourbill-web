ee746c3bc6a81ddc1446c5e32c0aca0451dce71c
Teng Wei Song <weisong.teng.work@gmail.com>
2020-03-28 17:09:25 +0800

    added send friend requests

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,50 +1,14 @@
 import axios from "axios";
-import qs from "qs";
+import { getInstance } from "../auth/index";
+
 
 export default {
-	/**
-	 * @param {Object} loginInfo
-	 * @param {String} loginInfo.username
-	 * @param {String} loginInfo.password
-	 * @returns {Promise} The accessInfo including access token and token type
-	 */
-    authenticate(loginInfo) {
-        // return axios.post(`${process.env.VUE_APP_GatewayApiUrl}/login`, loginInfo);
-        const param = {
-            client_id: process.env.VUE_APP_ClientId,
-            client_secret: process.env.VUE_APP_ClientSecret,
-            grant_type: "password",
-            username: loginInfo.username,
-            password: loginInfo.password
-        };
-
-        const config = {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        };
-        return axios.post(`${process.env.VUE_APP_IdentityServiceUrl}/connect/token`, qs.stringify(param), config);
-    },
-
-    /**
-     * @param {string} accessToken
-     * @returns {userInfo}
-     */
-    getCurrentUserInfo(accessToken) {
-        const config = {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        };
-
-        return axios.get(`${process.env.VUE_APP_IdentityServiceUrl}/connect/userinfo`, config);
-    },
-
     /**
      * @returns {Promise} The list of all users
      */
-    async getUsers(accessToken) {
-        console.log("access token", accessToken);
+    async getUsers() {
+        const authService = getInstance();
+        const accessToken = await authService.getTokenSilently();
         const config = {
             headers: {
                 Authorization: `Bearer ${accessToken}`
@@ -58,31 +22,32 @@ export default {
 	 * @param {Guid} requestorId 
 	 * @param {Guid} requesteeId 
 	 */
-    sendFriendRequest(requestorId, requesteeId) {
-        const request = { requestorId, requesteeId };
-        // console.log("request", request);
-        return axios.post(`${process.env.VUE_APP_GatewayApiUrl}/friend`, {
+    async sendFriendRequest(requestorId, requesteeId) {
+        const authService = getInstance();
+        const accessToken = await authService.getTokenSilently();
+        const config = {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        };
+        return axios.post(`${process.env.VUE_APP_Backend}/friendships`, {
             requestorId, requesteeId
-        });
+        }, config);
     },
 
 	/**
 	 * Get all friend requests
 	 * @param {Guid} requesteeId 
 	 */
-    getFriendRequests(requesteeId) {
-        return axios.get(`${process.env.VUE_APP_GatewayApiUrl}/friendRequests/${requesteeId}`);
-    },
-
-	/**
-	 * @param {Object} userInfo
-	 * @param {String} userInfo.username
-	 * @param {String} userInfo.email
-	 * @param {String} userInfo.password
-	 * @returns {Promise} The userInfo with id, username
-	 */
-    addNewUser(userInfo) {
-        return axios.post(`${process.env.VUE_APP_GatewayApiUrl}/signup`, userInfo);
+    async getFriendRequests(requesteeId) {
+        const authService = getInstance();
+        const accessToken = await authService.getTokenSilently();
+        const config = {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        };
+        return axios.get(`${process.env.VUE_APP_Backend}/friendships/requests/${requesteeId}`, config);
     },
 
 	/**
